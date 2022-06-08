ac9800cfeeb6f4e892fc949feba13968004daab9
Thomas Cook <tlncook85@gmail.com>
2022-06-08 15:26:27 -0600

    commit tree 8ea8ace88762723fa006e80843119495ed8f74fd

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return links.map((link) => {
+    return (
+        <a href={`#${link}`} key={link}>{link}</a>
+    );
+  })
 }
 
 export default NavBar;
